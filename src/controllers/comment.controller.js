ee516d96c6e82487a24493196199c1f67fc51a1c
Vaibhav Sharma <v.s.10060205@gmail.com>
2026-01-27 13:16:57 +0530

    Implement getVideoComments functionality with pagination and user lookup

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -7,7 +7,64 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    let {page = 1, limit = 10} = req.query
+
+    if(!videoId) {
+        throw new ApiError(400, "Video id is required")
+    }
+    
+    page = parseInt(page)
+    limit = parseInt(limit)
+
+    if(isNaN(page) || isNaN(limit)) {
+        throw new ApiError(400, "Pagination params must be numbers")
+    }
+
+    const pipeline = await Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $sort: { createdAt: -1 }   // doesn't update order in database, but in the return array
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner"
+            }
+        },
+        {
+            $unwind: "$owner"
+        },
+        {
+            $project: {
+                content: 1,
+                createdAt: 1,
+                "owner._id": 1,
+                "owner.username": 1,
+                "owner.avatar": 1
+            }   
+        }
+    ]);
+
+    const options = {
+        page,
+        limit
+    };
+
+    const result = await Comment.aggregatePaginate(pipeline, options);
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            result,
+            "Comments fetched successfully"
+        )
+    )
 
 })
 
