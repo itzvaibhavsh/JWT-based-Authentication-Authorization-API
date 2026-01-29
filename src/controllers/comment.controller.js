5184cc18cd2346174861f7baa673c7fdc291fb25
Vaibhav Sharma <v.s.10060205@gmail.com>
2026-01-29 20:52:47 +0530

    Implement deleteComment functionality with validation and authorization checks

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -151,11 +151,36 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params
+
+    if(!commentId) {
+        throw new ApiError(400, "Comment id is required")
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if(!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+
+    if(!comment.owner.equals(req.user._id)){
+        throw new ApiError(403, "You are not authorized to delete this comment")
+    }
+
+    await Comment.findByIdAndDelete(commentId)
+
+    return res.status(200).json(
+        new ApiResponse(
+        200,
+        {},
+        "Comment deleted successfully"
+        )
+    )
 })
 
 export {
     getVideoComments, 
     addComment, 
     updateComment,
-     deleteComment
+    deleteComment
     }
