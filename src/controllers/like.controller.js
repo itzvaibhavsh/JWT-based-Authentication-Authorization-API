199eed5cc9320242bf210090ccb9213e5ac4df75
Vaibhav Sharma <v.s.10060205@gmail.com>
2026-01-30 11:40:20 +0530

    Implement playlist controllers

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,22 +7,207 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+
+    const userId = req.user?._id
+
+    if(!videoId) {
+        throw new ApiError(400, "Video id is required")
+    }
+
+    if(!userId) {
+        throw new ApiError(401, "Unauthorized")
+    }
+
+    const existingLike = await Like.findOne({
+        video: videoId,
+        likedBy: userId
+    })
+    
+    let result
+    let message
+
+    if(existingLike) {
+        await Like.findByIdAndDelete(existingLike._id)
+
+        result = false
+        message = "Video unliked successfully"
+    } else {
+        await Like.create({
+            video: videoId,
+            likedBy: userId
+        })
+
+        result = true
+        message = "Video liked successfully"
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+        200,
+        {liked: result},
+        message
+        )
+    )
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    const userId = req.user?._id
 
+    if(!commentId) {
+        throw new ApiError(400, "Comment id is required")
+    }
+
+    if(!userId) {
+        throw new ApiError(401, "Unauthorized")
+    }
+
+    const existingLike = Like.findOne({
+        comment: commentId,
+        likedBy: userId
+    })
+    
+    let result
+    let message
+
+    if(existingLike) {
+        await Like.findByIdAndDelete(existingLike._id)
+
+        result = false
+        message = "Video unliked successfully"
+    } else {
+        await Like.create({
+            comment: commentId,
+            likedBy: userId
+        })
+
+        result = true
+        message = "Video liked successfully"
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+        200,
+        {liked: result},
+        message
+        )
+    )
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    const userId = req.user?._id
+
+    if(!tweetId) {
+        throw new ApiError(400, "Tweet id is required")
+    }
+
+    if(!userId) {
+        throw new ApiError(401, "Unauthorized")
+    }
+
+    const existingLike = Like.findOne({
+        tweet: tweetId,
+        likedBy: userId
+    })
+    
+    let result
+    let message
+
+    if(existingLike) {
+        await Like.findByIdAndDelete(existingLike._id)
+
+        result = false
+        message = "Video unliked successfully"
+    } else {
+        await Like.create({
+            tweet: tweetId,
+            likedBy: userId
+        })
+
+        result = true
+        message = "Video liked successfully"
+    }
+
+   return res.status(200).json(
+        new ApiResponse(
+        200,
+        {liked: result},
+        message
+        )
+    )
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const userId = req.user?._id
+
+    if(!userId) {
+        throw new ApiError(401, "Unauthorized")
+    }
+
+    let {page = 1, limit = 10} = req.query
+
+    const pipeline = Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(userId),
+                video: {$ne: null}
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "video"
+            }
+        },
+        {
+            $unwind: "$video"
+        },
+        {
+            $match: {
+                "video.isPublished": true
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                likedAt: "$createdAt",
+                videoId: "$video._id",
+                title: "$video.title",
+                thumbnail: "$video.thumbnail",
+                views: "$video.views",
+                duration: "$video.duration",
+                owner: "$video.owner"
+            }
+        }
+    ])
+
+    const options = {
+        page: Number(page),
+        limit: Number(limit)
+    }
+
+    const result = await Like.aggregatePaginate(pipeline, options)
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            result,
+            "Liked videos fetched successfully"
+        )
+    )
 })
 
 export {
--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -7,18 +7,102 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
-
+    
     //TODO: create playlist
+    if(!name || !name.trim()) {
+        throw new ApiError(400, "Playlist name is required")
+    }
+
+    if(!description || !description.trim()) {
+        throw new ApiError(400, "Playlist description is required")
+    }
+
+    const userId = req.user?._id
+
+    if(!userId) {
+        throw new ApiError(401, "Unauthorized")
+    }
+
+    const existingPlaylist = await Playlist.findOne({
+        owner: userId,
+        name: name.trim()
+    })
+
+    if(existingPlaylist) {
+        throw new ApiError(409, "Playlist with same name already exists")
+    }
+
+    const playlist = await Playlist.create({
+        name: name.trim(),
+        description: description.trim(),
+        owner: userId
+    })
+
+    if(!playlist) {
+        throw new ApiError(500, "Failed to create playlist")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(
+            201,
+            playlist,
+            "Playlist created successfully"
+        )
+    )
+
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+    if(!userId) {
+        throw new ApiError(400, "user id is required")
+    }
+
+    const playlists = await Playlist.find({
+        owner: userId
+    }).select("-__v").sort({createdAt: -1})
+
+    if(!playlists.length){
+        return res.status(200).json(
+            new ApiResponse(
+                200,
+                [],
+                "No playlists found"
+            )
+        )
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            playlists,
+            "Playlists fetched successfully by user"
+        )
+    )
+
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+    if(!playlistId) {
+        throw new ApiError(400, "Playlist id is required")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if(!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+        200,
+        playlist,
+        "Playlist fetched successfully by id"
+        )
+    )
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
